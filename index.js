9bdd78c3f1c58c92f0e147eef9bfd296e71e43a6
kavyangaHA <kavyangahathurusinghe1234@gmail.com>
2026-01-28 23:22:20 +0530

    did an initial testing

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express'); //import express module
 const app = express(); //create an express application
+
+app.get('/',(request,response)=>{
+    return response.send('Hello World');
+    //when user visits root directory, send 'Hello World' as response
+});
+
+app.listen(3000,()=>{
+    console.log('Server is running on http://Localhost:3000');
+    //start the server on port 3000 and log a message to the console
+})
